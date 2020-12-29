8b30be174531e7a8248ceae56df7c363ba54bd6f
k1rh4 <k1rha87@gmail.com>
2020-12-29 09:57:06 +0900

    Add Path

--- search_.py
+++ search_.py
@@ -1,7 +1,16 @@
 import glob 
 import os
+import sys 
+
 keywords = ["/bin/sh","popen","system","passw","token","secret", "dlopen","setreuid","upload","download","tar ","unzip"]
-for file_name in glob.glob("./*"):
+PATH=""
+if (len(sys.argv[1]) > 2):
+    PATH=sys.argv[1]
+
+else:
+    PATH="./"
+
+for file_name in glob.glob(PATH):
     print("[+] %s " %(file_name))
     for keyword in keywords :
         os.system("strings %s | grep %s | awk {'print \"\t\"$0'}" % (file_name, keyword)) 
